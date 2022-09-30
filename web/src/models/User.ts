929cba36dcdc2520e32f0d14df38aafce020070d
gabriellend <gdvdsn@gmail.com>
2022-09-30 09:15:06 -1000

    Add User mega class

---
+++ web/src/models/User.ts
@@ -0,0 +1,60 @@
+import axios, { AxiosResponse } from 'axios';
+
+interface UserProps {
+  name?: string;
+  age?: number;
+  id?: number;
+}
+
+type Callback = () => void;
+
+export class User {
+  // [key: string]: Callback – we don't know what keys we will
+  // have but we know they will be strings that point to Callbacks
+  events: { [key: string]: Callback[] } = {};
+
+  constructor(private data: UserProps) {}
+
+  get(propName: string): (string | number) {
+    return this.data[propName];
+  }
+
+  set(update: UserProps): void {
+    Object.assign(this.data, update);
+  }
+
+  on(eventName: string, callback: Callback): void {
+    const handlers = this.events[eventName] || [];
+    handlers.push(callback);
+    this.events[eventName] = handlers;
+  }
+
+  trigger(eventName: string): void {
+    const handlers = this.events[eventName];
+
+    if (!handlers || handlers.length === 0) {
+      return;
+    }
+
+    handlers.forEach(callback => {
+      callback();
+    });
+  }
+
+  fetch(): void {
+    axios.get(`http://localhost:3000/users/${this.get('id')}`)
+      .then((response: AxiosResponse): void => {
+        this.set(response.data);
+      });
+  }
+
+  save(): void {
+    const id = this.get('id');
+
+    if (id) {
+      axios.put(`http://localhost:3000/users/${id}`, this .data);
+    } else {
+      axios.post('http://localhost:3000/users', this.data);
+    }
+  }
+}
