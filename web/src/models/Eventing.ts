e210fe3ff6e9b92b1e4c9df2f4c1e24de0a2714a
gabriellend <gdvdsn@gmail.com>
2022-10-03 14:53:13 -1000

    Add passthrough methods

--- web/src/models/Eventing.ts
+++ web/src/models/Eventing.ts
@@ -4,14 +4,14 @@ export class Eventing {
   // [key: string]: Callback – we don't know what keys we will
   // have but we know they will be strings that point to Callbacks
   events: { [key: string]: Callback[] } = {};
-  
-  on(eventName: string, callback: Callback): void {
+
+  on = (eventName: string, callback: Callback): void => {
     const handlers = this.events[eventName] || [];
     handlers.push(callback);
     this.events[eventName] = handlers;
   }
 
-  trigger(eventName: string): void {
+  trigger = (eventName: string): void => {
     const handlers = this.events[eventName];
 
     if (!handlers || handlers.length === 0) {
