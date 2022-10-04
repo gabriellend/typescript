4a8b9f6e3d2ad222e624a70a81765a0d84f5ad23
gabriellend <gdvdsn@gmail.com>
2022-10-03 14:30:52 -1000

    Separate User into smaller classes

---
+++ web/src/models/Eventing.ts
@@ -0,0 +1,25 @@
+type Callback = () => void;
+
+export class Eventing {
+  // [key: string]: Callback – we don't know what keys we will
+  // have but we know they will be strings that point to Callbacks
+  events: { [key: string]: Callback[] } = {};
+  
+  on(eventName: string, callback: Callback): void {
+    const handlers = this.events[eventName] || [];
+    handlers.push(callback);
+    this.events[eventName] = handlers;
+  }
+
+  trigger(eventName: string): void {
+    const handlers = this.events[eventName];
+
+    if (!handlers || handlers.length === 0) {
+      return;
+    }
+
+    handlers.forEach(callback => {
+      callback();
+    });
+  }
+}
--- web/src/models/User.ts
+++ web/src/models/User.ts
@@ -1,60 +1,20 @@
-import axios, { AxiosResponse } from 'axios';
+import { Eventing } from './Eventing';
+import { Sync } from './Sync';
+import { Attributes } from './Attributes';
 
-interface UserProps {
+export interface UserProps {
+  id?: number;
   name?: string;
   age?: number;
-  id?: number;
 }
 
-type Callback = () => void;
-
+const rootUrl = 'http://localhost:3000/users';
 export class User {
-  // [key: string]: Callback – we don't know what keys we will
-  // have but we know they will be strings that point to Callbacks
-  events: { [key: string]: Callback[] } = {};
+  events: Eventing = new Eventing();
+  sync: Sync<UserProps> = new Sync<UserProps>(rootUrl);
+  attributes: Attributes<UserProps>;
 
-  constructor(private data: UserProps) {}
-
-  get(propName: string): (string | number) {
-    return this.data[propName];
-  }
-
-  set(update: UserProps): void {
-    Object.assign(this.data, update);
-  }
-
-  on(eventName: string, callback: Callback): void {
-    const handlers = this.events[eventName] || [];
-    handlers.push(callback);
-    this.events[eventName] = handlers;
-  }
-
-  trigger(eventName: string): void {
-    const handlers = this.events[eventName];
-
-    if (!handlers || handlers.length === 0) {
-      return;
-    }
-
-    handlers.forEach(callback => {
-      callback();
-    });
-  }
-
-  fetch(): void {
-    axios.get(`http://localhost:3000/users/${this.get('id')}`)
-      .then((response: AxiosResponse): void => {
-        this.set(response.data);
-      });
-  }
-
-  save(): void {
-    const id = this.get('id');
-
-    if (id) {
-      axios.put(`http://localhost:3000/users/${id}`, this .data);
-    } else {
-      axios.post('http://localhost:3000/users', this.data);
-    }
+  constructor(attrs: UserProps) {
+    this.attributes = new Attributes<UserProps>(attrs);
   }
 }
