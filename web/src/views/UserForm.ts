2505fe04409612849931c9fd8bc85a743cd673ea
gabriellend <gdvdsn@gmail.com>
2022-10-04 15:19:56 -1000

    Make onSetNameClick

--- web/src/views/UserForm.ts
+++ web/src/views/UserForm.ts
@@ -16,12 +16,20 @@ export class UserForm {
 
   eventsMap(): { [key: string]: () => void } {
     return {
-      'click:.set-age': this.onSetAgeClick
+      'click:.set-age': this.onSetAgeClick,
+      'click:.set-name': this.onSetNameClick
     };
   }
 
   onSetAgeClick = (): void => {
     this.model.setRandomAge();
+  }
+
+  onSetNameClick = (): void => {
+    const input= this.parent.querySelector('input') as HTMLInputElement;
+    const name = input.value;
+
+    this.model.set({ name });
   }
 
   template(): string {
@@ -31,7 +39,7 @@ export class UserForm {
         <div>User name: ${this.model.get('name')}</div>
         <div>User age: ${this.model.get('age')}</div>
         <input />
-        <button>Click Me</button>
+        <button class="set-name">Change Name</button>
         <button class="set-age">Set Random Age</button>
       </div>
     `
@@ -51,7 +59,7 @@ export class UserForm {
 
   render(): void {
     this.parent.innerHTML = '';
-    
+
     const templateElement = document.createElement('template');
     templateElement.innerHTML = this.template();
 
