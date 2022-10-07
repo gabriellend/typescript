24b6e0c84614cc8a8bd5a9b8ac09c3919cc452d4
gabriellend <gdvdsn@gmail.com>
2022-10-06 15:52:12 -1000

    Add middleware

--- server/src/routes/loginRoutes.ts
+++ server/src/routes/loginRoutes.ts
@@ -1,9 +1,38 @@
 import { Router, Request, Response } from 'express';
 
+// When type definitions files aren't that good, you can
+// create an interface to extend the provided types
+interface RequestWithBody extends Request {
+  body: { [key:string]: string | undefined}
+}
+
 const router = Router();
 
-router.get('/', (req: Request, res: Response) => {
-  res.send('hi there');
+router.get('/login', (req: Request, res: Response) => {
+  res.send(`
+    <form method="POST">
+      <div>
+        <label>Email</label>
+        <input name="email" />
+      </div>
+      <div>
+        <label>Password</label>
+        <input name="password" type="password" />
+      </div>
+      <button>Submit</button>
+    </form>
+  `);
+});
+
+router.post('/login', (req: RequestWithBody, res: Response) => {
+  const { email, password } = req.body;
+
+  if (email && password && email === 'hi@hi.com' && password === 'password') {
+    req.session = { loggedIn: true };
+    res.redirect('/');
+  } else {
+    res.send('Invalid email or password');
+  }
 });
 
 export { router };
