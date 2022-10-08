9b7126574c5043e93a9e8db27c82554d986a47d3
gabriellend <gdvdsn@gmail.com>
2022-10-07 14:35:03 -1000

    Functional component example

--- rrts/src/index.tsx
+++ rrts/src/index.tsx
@@ -9,32 +9,33 @@ interface AppState {
   counter: number;
 }
 
-class App extends React.Component<AppProps, AppState > {
-  constructor(props: AppProps) {
-    super(props);
-    this.state = { counter: 0 };
-  }
+const App = (props: AppProps): JSX.Element => {
+  return <div>{props.color}</div>
+};
 
-  onIncrement = (): void => {
-    this.setState({ counter: this.state.counter + 1 });
-  }
+// class App extends React.Component<AppProps> {
+//   state = { counter: 0 };
 
-  onDecrement = (): void => {
-    this.setState({ counter: this.state.counter - 1 });
-  }
+//   onIncrement = (): void => {
+//     this.setState({ counter: this.state.counter + 1 });
+//   }
 
-  render() {
-    return (
-      <div>
-        <button onClick={this.onIncrement}>Increment</button>
-        <button onClick={this.onDecrement}>Decrement</button>
-        {this.state.counter}
-      </div>
-    );
-  }
-}
+//   onDecrement = (): void => {
+//     this.setState({ counter: this.state.counter - 1 });
+//   }
+
+//   render() {
+//     return (
+//       <div>
+//         <button onClick={this.onIncrement}>Increment</button>
+//         <button onClick={this.onDecrement}>Decrement</button>
+//         {this.state.counter}
+//       </div>
+//     );
+//   }
+// }
 
 ReactDOM.render(
-  <App />,
+  <App color="red"/>,
   document.querySelector('#root')
 )
