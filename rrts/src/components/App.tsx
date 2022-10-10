83a2afaf9729157a557f3ebd7a00f0fe7e451729
gabriellend <gdvdsn@gmail.com>
2022-10-10 11:14:00 -1000

    Wire up reducer

--- rrts/src/components/App.tsx
+++ rrts/src/components/App.tsx
@@ -1,7 +1,19 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { Todo, fetchTodos } from '../actions';
+import { StoreState } from '../reducers';
 
-export class App extends React.Component {
+interface AppProps {
+  todos: Todo[];
+  fetchTodos(): any;
+}
+
+export class App extends React.Component<AppProps> {
   render() {
     return <div>Hi there!</div>
   }
 }
+
+const mapStateToProps = (state: StoreState): { todos: Todo[] } => {
+  return { todos: state.todos };
+};
--- rrts/src/reducers/index.ts
+++ rrts/src/reducers/index.ts
@@ -1,5 +1,11 @@
 import { combineReducers } from 'redux';
+import { todosReducer } from './todos';
+import { Todo } from '../actions';
 
-export const reducers = combineReducers({
-  counter: () => 1
-})
+export interface StoreState { // This can represent the entire store
+  todos: Todo[]; //<-- typescript is validating
+}
+
+export const reducers = combineReducers<StoreState>({
+  todos: todosReducer //<-- that this is returning the expected value
+});
