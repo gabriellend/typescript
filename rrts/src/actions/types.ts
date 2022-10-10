2528ebd78aedf7767e478a4c7ea9a38f083b6bb0
gabriellend <gdvdsn@gmail.com>
2022-10-10 12:09:54 -1000

    Break up actions and add methods

--- rrts/src/actions/types.ts
+++ rrts/src/actions/types.ts
@@ -1,6 +1,10 @@
+import { FetchTodosAction, DeleteTodoAction } from "./todos";
 // If you don't write fetchTodos = 'FETCH_TODOS', typescript
 // will assign indexes to each value like fetchTodos = 0. It 
 // doesn't matter, it just has to be unique
 export enum ActionTypes {
-  fetchTodos
+  fetchTodos,
+  deleteTodo
 }
+
+export type Action = FetchTodosAction | DeleteTodoAction;
--- rrts/src/components/App.tsx
+++ rrts/src/components/App.tsx
@@ -1,19 +1,44 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { Todo, fetchTodos } from '../actions';
+import { Todo, fetchTodos, deleteTodo } from '../actions';
 import { StoreState } from '../reducers';
 
 interface AppProps {
   todos: Todo[];
-  fetchTodos(): any;
+  fetchTodos: typeof fetchTodos;
+  deleteTodo: typeof deleteTodo;
 }
 
-export class App extends React.Component<AppProps> {
+class _App extends React.Component<AppProps> {
+  onButtonClick = (): void => {
+    this.props.fetchTodos();
+  };
+
+  onTodoClick = (id: number): void => {
+    this.props.deleteTodo(id);
+  };
+
+  renderList(): JSX.Element[] {
+    return this.props.todos.map((todo: Todo) => {
+      return <div onClick={() => this.onTodoClick(todo.id)} key={todo.id}>{todo.title}</div>
+    });
+  }
+
   render() {
-    return <div>Hi there!</div>
+    return (
+      <div>
+        <button onClick={this.onButtonClick}>Fetch</button>
+        {this.renderList()}
+      </div>
+    );
   }
 }
 
 const mapStateToProps = ({ todos }: StoreState): { todos: Todo[] } => {
   return { todos };
 };
+
+export const App = connect(
+  mapStateToProps, 
+  { fetchTodos, deleteTodo }
+)(_App);
