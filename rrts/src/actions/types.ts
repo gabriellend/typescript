924508ca41b3d3bb138f54001b01b057b0de70af
gabriellend <gdvdsn@gmail.com>
2022-10-10 10:45:37 -1000

    Add typescript to action

---
+++ rrts/src/actions/types.ts
@@ -0,0 +1,6 @@
+// If you don't write fetchTodos = 'FETCH_TODOS', typescript
+// will assign indexes to each value like fetchTodos = 0. It 
+// doesn't matter, it just has to be unique
+export enum ActionTypes {
+  fetchTodos
+}
